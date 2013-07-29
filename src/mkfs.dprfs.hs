f5a0adc7ae6c6a00852d9cac63d25ad96d1b3e10
Brian Coca <briancoca+dev@gmail.com>
2013-07-28 21:38:12 -0400

    1/2 way there Signed-off-by: Brian Coca <briancoca+dev@gmail.com>

--- src/mkfs.dprfs.hs
+++ src/mkfs.dprfs.hs
@@ -2,17 +2,19 @@ import Data.ConfigFile
 import Control.Monad.Error
 import System.Directory
 import System.Environment
+import System.Posix.Process
+import System.Posix.Files
+import System.IO
 
 usage :: IO ()
 usage = do
-    name <- getProgName
-    putStrLn $ "Usage: " ++ name ++ " <path>"
+    me <- getProgName
+    putStrLn $ "Usage: " ++ me ++ " <path>"
 
 getconfig :: IO ()
 getconfig = do
     rv <- runErrorT $
         do
-        cp <- join $ liftIO $ readfile emptyCP "server.conf"
         let x = cp
         buffersize <- get x  "general" "buffer_size"
         liftIO $ putStrLn buffersize
@@ -22,7 +24,6 @@ getconfig = do
         liftIO $ putStrLn data_root
     print rv
 
-
 main :: IO ()
 main = do
     args <- getArgs
@@ -30,5 +31,34 @@ main = do
         then usage
         else do
             cwd <- getCurrentDirectory :: IO FilePath
-            getconfig
-            print args
+
+            -- TODO: figure out monad error handling
+            -- get settings
+            cp <- join $ liftIO $ readfile emptyCP "server.conf"
+            buffersize <- get cp "general" "buffer_size"
+            meta_root  <- get cp "status"  "meta_root"
+            data_root  <- get cp "data"    "data_root"
+
+            newdir <- createDirectoryIfMissing meta_root ++ "/" ++ args
+            setCurrentDirectory newdir
+
+            content <- getContents stdin
+            -- loop over bytes from std in (recursive func??)
+            -- break if isEOF
+            -- hash them
+            -- hash dir, creat if needed
+            -- write file fragment
+            -- next!
+
+            setCurrentDirectory cwd
+            pid <- getProcessID
+
+            let link_name = newdir ++ "/" ++ "base"
+            let t_link_name = link_name ++ "." ++ pid
+            createSymbolickLink base t_link_name
+            rename t_link_name link_name
+            
+            let link_name = newdir ++ "/" ++ "top"
+            let t_link_name = link_name ++ "." ++ pid
+            createSymbolickLink base t_link_name
+            rename t_link_name link_name
